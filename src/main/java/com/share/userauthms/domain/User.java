f17420f8bc16a04ec9381b1b49157a67f72e8a47
Devoamos1 <devonteyeah@gmail.com>
2023-10-26 10:38:42 +0100

    Swagger config and base user domain.

--- src/main/java/com/share/userauthms/domain/User.java
+++ src/main/java/com/share/userauthms/domain/User.java
@@ -1,11 +1,38 @@
 package com.share.userauthms.domain;
 
-import lombok.Data;
-
-@Data
 public class User {
+    public User(String userName) {
+        this.userName = userName;
+    }
 
-    private String name;
+    private String userName;
 
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+
+        return getUserName() != null ? getUserName().equals(user.getUserName()) : user.getUserName() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getUserName() != null ? getUserName().hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                '}';
+    }
 }
 
--- src/main/java/com/share/userauthms/security/WebSecurityConfig.java
+++ src/main/java/com/share/userauthms/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.share.userauthms.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,14 +11,29 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
+    @Value("${spring.config.activate.on-profile}")
+    private String activeProfile;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain web(HttpSecurity http) throws Exception {
+        if ("local".equals(activeProfile)) {
+            // Configure security for the 'local' profile (e.g., disable authentication)
+            http
+                    .authorizeHttpRequests(requests -> requests
+                            .requestMatchers("/*", "/actuator/health", "/swagger", "/v2/api-docs", "/v3/api-docs/**", "/swagger-ui-custom.html").permitAll()
+                            .anyRequest().authenticated()
+                    );
+            return http.build();
+        }
+
         http
-                .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/", "/swagger-ui-custom.html").permitAll()
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/*").permitAll()
                         .anyRequest().authenticated()
                 );
+        // ...
+        // Disable CSRF for development/testing
+
         return http.build();
     }
 
