fdeb81b66ac8484e214d38765565d2a421d98ad1
Devonte Amos <devonteyeah@gmail.com>
2023-11-30 11:29:15 +0000

    BLOG-17 - Create CI/CD pipeline (#2)
    * Create gradle.yml
    
    Base workflow file.
    
    * Adding artifact check and build
    
    * Logging test cases and vulnerability update.
    
    * Adding initial build reusable workflow
    
    * Switching on push/pr to main
    
    * Updating reference
    
    * Updates to pull request runs.
    
    * Updating Java 17.
    
    * Refactoring.
    
    * Refactoring.
    
    * Update latest spring version.
    
    * Adding jacoco test coverage.
    
    * Testing permission changes
    
    * Testing various jacoco actions
    
    * Clean up - changes added to reusable workflow.
    
    * Secrets amendment.
    
    * Amending workflow to main branch
    
    ---------
    
    Co-authored-by: Devoamos1 <devonteamos@hotmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-	id("org.springframework.boot") version "3.1.4" // Your specified version
+	id("org.springframework.boot") version "3.1.5" // Your specified version
 	id("io.spring.dependency-management") version "1.1.3" // Your specified version
+	jacoco
 	kotlin("jvm") version "1.9.10" // Adjust this to your Kotlin version
 }
 
@@ -13,6 +14,11 @@ version = "0.0.1"
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+jacoco {
+	toolVersion = "0.8.11"
+	reportsDirectory = layout.buildDirectory.dir("customJacocoReportDir")
 }
 
 repositories {
@@ -29,13 +35,12 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.cloud:spring-cloud-dependencies:${springCloudVersion}")
 
-
 	// Your specified versions
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springDocVersion")
 
 	// Other dependencies
 	implementation("org.springframework.boot:spring-boot-devtools")
-	implementation("org.webjars:jquery:3.4.1")
+	implementation("org.webjars:jquery:3.7.1")
 	implementation("org.webjars:bootstrap:4.3.1")
 	implementation("org.webjars:webjars-locator-core")
 
@@ -49,4 +54,20 @@ dependencies {
 
 tasks.test {
 	useJUnitPlatform()
+
+	testLogging {
+		events("passed")
+	}
+
+	finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test) // tests are required to run before generating the report
+
+	reports {
+		xml.required = false
+		csv.required = false
+		html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
+	}
 }
